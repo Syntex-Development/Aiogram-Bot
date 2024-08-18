dd409f5c602cae99ad8cf70bddc23b976df9141c
consisteda <consisteda@gmail.com>
2024-08-18 13:34:25 +0300

    TEXT

---
+++ middlewares.py
@@ -0,0 +1,19 @@
+from aiogram import BaseMiddleware
+from aiogram.dispatcher.event.bases import UNHANDLED
+from unittest.mock import sentinel
+from typing import Callable, Dict, Any, Awaitable
+from aiogram.types import TelegramObject
+
+
+class TestMiddleware1(BaseMiddleware):
+    async def __call__(self, handler: Callable [[TelegramObject, Dict[str, Any]], Awaitable[Any]], event: TelegramObject, data: Dict [str, Any]):
+        result = await handler(event, data)
+        await event.answer()
+        return result
+
+class TestMiddleware2(BaseMiddleware):
+    async def __call__(self, handler: Callable [[TelegramObject, Dict[str, Any]], Awaitable[Any]], event: TelegramObject, data: Dict [str, Any]):
+        result = await handler(event, data)
+        if result == sentinel.UNHANDLED:
+            pass
+        return result
---
+++ run.py
@@ -0,0 +1,27 @@
+from aiogram import Dispatcher, Bot
+from aiogram.client.default import DefaultBotProperties
+import asyncio, logging
+
+from app.handlers import router
+from app.database.models import create_db, delete_db
+from config import config
+
+
+
+async def main():
+    # await delete_db()
+    await create_db()
+    bot = Bot(token=config.TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
+    dp = Dispatcher()
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, skip_updates=True)
+    
+    
+    
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
